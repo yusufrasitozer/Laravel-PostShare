6f02dd0cf43c862d225d526eaa4855e8a5a9ee4c
yusuf <yusufozer1030@gmail.com>
2021-12-22 00:46:31 +0300

    Uploading all project

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+
+class RegisterController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware(['guest']);
+    }
+
+
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request) 
+    {
+
+        //validaton
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+        ]);
+
+        //store user
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        auth()->attempt($request->only('email', 'password'));
+        
+        return redirect()->route('dashboard');
+
+        //sign the user in
+        //redirect
+    }
+}
